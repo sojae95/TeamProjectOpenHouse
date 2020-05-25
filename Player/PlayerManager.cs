7889219fc16ec6ee8a0c8be869e62f8678068a35
KimTaeJun <zzaxowns@naver.com>
2020-05-26 00:07:51 +0900

    2020.05.26 커밋
    확인차 한번 들어가서 어떻게 만들어야 할지 보고  Player 스크립트 만들었습니다.
    
    애니메이션부분 봤는데, 애니메이터 컨트롤러 만들다가 저 죽을지도 모를거 같습니다.
    
    조금씩이지만 시간날때마다 작업해보겠습니다.

---
+++ Player/PlayerManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    //플레이어 이동:  FPS - 전후좌우(조이스틱) + 뛰기(보류) + 앉기(최대한 마지막), 점프, 총 쏘기
+    //플레이어 애니메이션:  기본적으로 있는거 + 앉기(제작 - 최대한 마지막에), 총관련 애니메이션
+    //Player Manager(스크립트) 만들어서 플레이어에 관한 내용들 넣어두고 GM에서 관리
+    //Player Manager 스크립트에 들어갈 내용: HP, 속도, 키입력, 키입력에 따른 애니메이션
+
+    public enum PlayerState
+    {
+        idle,
+        walk,
+        shoot
+        // 앉기, 뛰기(보류)
+    }
+
+    PlayerState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = PlayerState.idle;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void playerMove() { 
+    
+    }
+
+    void shootGun() { 
+    
+    }
+}
