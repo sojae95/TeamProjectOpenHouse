f1c6e1b6ae67b2085758e7c03e47ec5bede0b66f
KimTaeJun <zzaxowns@naver.com>
2020-06-17 20:26:14 +0900

    플레이어 데모버전입니다.
    플레이어 데모버전입니다. 움직이는거랑 애니메이션 엮어놨습니다.

---
+++ Player/FollowCam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform target;
+    private float dist = 0;
+    private float height = 0;
+    private float dampTrace = 20.0f;
+  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camPosUpdate();
+    }
+
+    void camPosUpdate() {
+        //transform.position = Vector3.Lerp(target.position - (target.forward * +dist) + (Vector3.up * height), transform.position,
+        //     Time.deltaTime * dampTrace);
+
+        transform.rotation = target.rotation;
+
+        transform.LookAt(target.position);
+    }
+}
--- Player/PlayerManager.cs
+++ Player/PlayerManager.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
 
-    //플레이어 이동:  FPS - 전후좌우(조이스틱) + 뛰기(보류) + 앉기(최대한 마지막), 점프, 총 쏘기
-    //플레이어 애니메이션:  기본적으로 있는거 + 앉기(제작 - 최대한 마지막에), 총관련 애니메이션
-    //Player Manager(스크립트) 만들어서 플레이어에 관한 내용들 넣어두고 GM에서 관리
-    //Player Manager 스크립트에 들어갈 내용: HP, 속도, 키입력, 키입력에 따른 애니메이션
+    //플레이어 이동
+    //마우스이동 및 FPS 구도 만들기
+    //플레이어 이동 애니메이션 추가
+
+    private float moveSpeed = 3;
+    private Animator playerAnimator;
 
     public enum PlayerState
     {
         idle,
-        walk,
-        shoot
+        walk
         // 앉기, 뛰기(보류)
     }
 
@@ -24,18 +25,45 @@ public class PlayerManager : MonoBehaviour
     void Start()
     {
         state = PlayerState.idle;
+        playerAnimator = GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
     {
-
+        checkState();
+        checkAnimation();
+        playerMove();
     }
 
-    void playerMove() { 
-    
+    void playerMove() {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(h, 0, v) * moveSpeed * Time.deltaTime);
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
     }
 
     void shootGun() { 
     
+    }
+
+    void checkState() {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W))
+        {
+            state = PlayerState.walk;
+        }
+        else {
+            state = PlayerState.idle;
+        }
+    }
+
+    void checkAnimation() {
+        if (state == PlayerState.walk)
+        {
+            playerAnimator.SetBool("isWalk", true);
+        }
+        else {
+            playerAnimator.SetBool("isWalk", false);
+        }
     }
 }
