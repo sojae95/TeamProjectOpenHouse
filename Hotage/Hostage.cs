fe7e6340975c0c9feaa261990ae457adaf25cb64
Hyuneon <37572040+HyuneonY@users.noreply.github.com>
2020-06-19 14:51:16 +0900

    인질 ai추가

---
+++ Hotage/Hostage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Hostage : MonoBehaviour
+{
+    public GameObject target; //따라갈 타겟
+    public GameObject Goal;
+    NavMeshAgent agent;
+    bool moveStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        moveStart = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveStart == true)
+        {
+            agent.destination = target.transform.position;
+        }
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Goal")
+        {
+            moveStart = false;
+        }
+        else if (other.tag == "Player")
+        {
+            moveStart = true;
+        }    
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            moveStart = false;
+        }
+    }
+}
