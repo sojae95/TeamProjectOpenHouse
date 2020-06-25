7e6c88d6b6484d729b318419d796720def996225
KimTaeJun <zzaxowns@naver.com>
2020-06-25 19:58:58 +0900

    플레이어 애니메이션 부분 수정했습니다.
    기본 상황에서 리로드 혹은 걷는 상황에서 리로드까지는 에러없이 만들어졌는데, 뛰는 동안 리로드하게는 만들지 못했습니다. 근데 만들다보니 뛰면서 리로드 한다는게 말이 되나 싶기도 하네요. 회의때 말씀드립니다.

--- Player/PlayerManager.cs
+++ Player/PlayerManager.cs
@@ -10,6 +10,7 @@ public class PlayerManager : MonoBehaviour
     //마우스 상하로 움직이는거 최대한 완성시켜 봅니다.(모델링이 이상해서 지금은 넣어놓지 않았습니다. )
 
     public static float moveSpeed = 5;
+    public static float rotSpeed = 3;
 
     private Animator playerAnimator;
 
@@ -42,7 +43,9 @@ public class PlayerManager : MonoBehaviour
         float v = Input.GetAxis("Vertical");
 
         transform.Translate(new Vector3(h, 0, v) * moveSpeed * Time.deltaTime);
+
         transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
+
     }
 
     void shootGun() { 
@@ -53,81 +56,76 @@ public class PlayerManager : MonoBehaviour
     {
         Debug.Log(state);
 
+
         if (state == PlayerState.idle)
         {
-            // 기본 모습에서 움직이는 키 누르면 Walk 상태로 바꿈
-
-            if (Input.GetKey(KeyCode.R))
+            if (Input.GetKeyDown(KeyCode.R))
             {
                 playerAnimator.SetBool("isReLoad", true);
                 state = PlayerState.reload;
             }
-            else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S)
-                || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W))
+            else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W))
             {
                 playerAnimator.SetBool("isWalk", true);
                 state = PlayerState.walk;
             }
-
         }
-        else if (state == PlayerState.walk)
-        {
-            //walk 상태에서 LeftShift를 누르면 뜀
-            if (Input.GetKey(KeyCode.R))
+        else if (state == PlayerState.walk){
+            if (Input.GetKeyDown(KeyCode.R))
             {
                 playerAnimator.SetBool("isReLoad", true);
                 state = PlayerState.reload;
             }
-            else if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S)|| Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W)) && Input.GetKey(KeyCode.LeftShift))
+            else if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W)) && Input.GetKey(KeyCode.LeftShift))
             {
-                playerAnimator.SetBool("isWalk", false);
+                //playerAnimator.SetBool("isWalk", false);
                 playerAnimator.SetBool("isRun", true);
                 state = PlayerState.run;
             }
-            else if (!(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W)))
+            else if (!(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W)))
             {
-                // 만약  어떠한 이동키도 누르고 있지 않다면 idle로 넘어감
                 playerAnimator.SetBool("isWalk", false);
                 state = PlayerState.idle;
             }
-  
 
-        }
-        else if (state == PlayerState.run)
-        {
-            if (Input.GetKey(KeyCode.R))
+        } 
+        else if (state == PlayerState.run){
+            if (Input.GetKeyDown(KeyCode.R))
             {
                 playerAnimator.SetBool("isReLoad", true);
+                playerAnimator.SetBool("isRun", false);
                 state = PlayerState.reload;
             }
-            else if (!Input.GetKey(KeyCode.LeftShift) && (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W)))
+            else if (!Input.GetKey(KeyCode.LeftShift) && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W)))
             {
                 playerAnimator.SetBool("isRun", false);
                 playerAnimator.SetBool("isWalk", true);
                 state = PlayerState.walk;
             }
-            else if (!(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W)))
+            else if (!(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W)))
             {
+                playerAnimator.SetBool("isWalk", false);
                 playerAnimator.SetBool("isRun", false);
                 state = PlayerState.idle;
             }
 
         }
-        else if (state == PlayerState.reload) {
+        else if (state == PlayerState.reload){
             //playerAnimator.SetBool("isReLoad", false);
             //state = PlayerState.idle;
 
-            //Debug.Log("리로드 진행중");
+            Debug.Log("리로드 진행중");
             if (!playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("ReloadOutOfAmmo"))
             {
                 playerAnimator.SetBool("isReLoad", false);
                 state = PlayerState.idle;
                 Debug.Log("리로드  끝");
-
+                   
             }
 
         }
-        
+
+
     }
 
 }
