1382434b42199fb33f256865ff267c27cbc86330
KimTaeJun <zzaxowns@naver.com>
2020-06-26 13:56:23 +0900

    플레이어 움직이는거 완성
    플레이어 움직이는 부분 상하 마우스 회전 부분 문제 있던거 처리했습니다.

--- Player/PlayerManager.cs
+++ Player/PlayerManager.cs
@@ -9,17 +9,22 @@ public class PlayerManager : MonoBehaviour
     //플레이어 달리기, 리로딩, 총쏘기 스테이터스 및 애니메이션 추가
     //마우스 상하로 움직이는거 최대한 완성시켜 봅니다.(모델링이 이상해서 지금은 넣어놓지 않았습니다. )
 
+
+
     public static float moveSpeed = 5;
-    public static float rotSpeed = 3;
+    public static float rotSpeed = 60;
 
     private Animator playerAnimator;
-
+    private Transform Playertr;
     public enum PlayerState
     {
         idle,
         walk,
         run,
-        reload
+        reload,
+        fire,
+        aimming,
+        aimfire
         // 앉기, 뛰기(보류)
     }
 
@@ -30,29 +35,25 @@ public class PlayerManager : MonoBehaviour
     {
         state = PlayerState.idle;
         playerAnimator = GetComponent<Animator>();
+        Playertr = GetComponent<Transform>();
     }
     // Update is called once per frame
     void Update()
     {
-        checkAnimation();
+        Animation();
         playerMove();
     }
 
     void playerMove() {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        transform.Translate(new Vector3(h, 0, v) * moveSpeed * Time.deltaTime);
-
-        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
-
+        float rZ = Input.GetAxis("Mouse Y");
+        Playertr.Rotate(Vector3.left * rotSpeed * Time.deltaTime * rZ);
     }
 
     void shootGun() { 
     
     }
 
-    void checkAnimation()
+    void Animation()
     {
         Debug.Log(state);
 
@@ -64,17 +65,38 @@ public class PlayerManager : MonoBehaviour
                 playerAnimator.SetBool("isReLoad", true);
                 state = PlayerState.reload;
             }
+            else if (Input.GetMouseButton(1))
+            {
+                playerAnimator.SetBool("isAimming", true);
+                state = PlayerState.aimming;
+            }
             else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W))
             {
                 playerAnimator.SetBool("isWalk", true);
                 state = PlayerState.walk;
             }
+            else if (Input.GetMouseButton(0))
+            {
+                playerAnimator.SetBool("isFire", true);
+                state = PlayerState.fire;
+            }
         }
-        else if (state == PlayerState.walk){
+        else if (state == PlayerState.walk)
+        {
             if (Input.GetKeyDown(KeyCode.R))
             {
                 playerAnimator.SetBool("isReLoad", true);
                 state = PlayerState.reload;
+            }
+            else if (Input.GetMouseButton(1))
+            {
+                playerAnimator.SetBool("isAimming", true);
+                state = PlayerState.aimming;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                playerAnimator.SetBool("isFire", true);
+                state = PlayerState.fire;
             }
             else if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W)) && Input.GetKey(KeyCode.LeftShift))
             {
@@ -87,16 +109,17 @@ public class PlayerManager : MonoBehaviour
                 playerAnimator.SetBool("isWalk", false);
                 state = PlayerState.idle;
             }
-
-        } 
-        else if (state == PlayerState.run){
-            if (Input.GetKeyDown(KeyCode.R))
+            else if (Input.GetMouseButton(0))
             {
-                playerAnimator.SetBool("isReLoad", true);
-                playerAnimator.SetBool("isRun", false);
-                state = PlayerState.reload;
+                playerAnimator.SetBool("isFire", true);
+                state = PlayerState.fire;
             }
-            else if (!Input.GetKey(KeyCode.LeftShift) && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W)))
+
+        }
+        else if (state == PlayerState.run)
+        {
+
+            if (!Input.GetKey(KeyCode.LeftShift) && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W)))
             {
                 playerAnimator.SetBool("isRun", false);
                 playerAnimator.SetBool("isWalk", true);
@@ -110,19 +133,29 @@ public class PlayerManager : MonoBehaviour
             }
 
         }
-        else if (state == PlayerState.reload){
-            //playerAnimator.SetBool("isReLoad", false);
-            //state = PlayerState.idle;
-
-            Debug.Log("리로드 진행중");
-            if (!playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("ReloadOutOfAmmo"))
+        else if (state == PlayerState.reload)
+        {
+            playerAnimator.SetBool("isReLoad", false);
+            state = PlayerState.idle;
+        }
+        else if (state == PlayerState.fire)
+        {
+            if (!Input.GetMouseButton(0))
             {
-                playerAnimator.SetBool("isReLoad", false);
+                playerAnimator.SetBool("isFire", false);
+                playerAnimator.SetBool("isWalk", false);
+                playerAnimator.SetBool("isRun", false);
                 state = PlayerState.idle;
-                Debug.Log("리로드  끝");
-                   
-            }
 
+            }
+        }
+        else if (state == PlayerState.aimming) {
+            if (!Input.GetMouseButton(1))
+            {
+                playerAnimator.SetBool("isAimming", false);
+                Debug.Log("에이밍 풀림");
+                state = PlayerState.idle;
+            }
         }
 
 
---
+++ Player/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public static float moveSpeed = 5;
+    public static float rotSpeed = 60;
+
+    private Transform Playertr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Playertr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float rY = Input.GetAxis("Mouse X");
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+        Playertr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+        Playertr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * rY);
+    }
+}
