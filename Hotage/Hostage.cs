d8eb572ba59d07936319b43435d1c37b965ac7ad
Hyuneon <37572040+HyuneonY@users.noreply.github.com>
2020-06-27 13:48:57 +0900

    인질 수정 및 UI추가

--- Hotage/Hostage.cs
+++ Hotage/Hostage.cs
@@ -2,18 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Hostage : MonoBehaviour
 {
     public GameObject target; //따라갈 타겟
     public GameObject Goal;
     NavMeshAgent agent;
-    bool moveStart;
+    public static bool moveStart;
+    Animator animator;
+    Vector3 lookDirection;
+    [SerializeField] private Slider slider;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        moveStart = false;  
+        moveStart = false;
+        animator = GetComponentInChildren<Animator>();
+        slider.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,10 +28,16 @@ public class Hostage : MonoBehaviour
     {
         if (moveStart == true)
         {
-            agent.destination = target.transform.position;
+            agent.destination = target.transform.position - Vector3.back;
         }
+        if (Vector3.Distance(this.transform.position, target.transform.position) < 1)
+        {
+            lookDirection = target.transform.position;
+            lookDirection.y = 0.0f;
+            transform.LookAt(lookDirection);
+        }
+        animator.SetFloat("Speed", agent.velocity.magnitude);
 
-       
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -35,14 +48,40 @@ public class Hostage : MonoBehaviour
         }
         else if (other.tag == "Player")
         {
-            moveStart = true;
-        }    
+            if (slider.value != 1)
+            {
+                slider.gameObject.SetActive(true);
+            }
+            else
+            {
+                moveStart = true;
+            }
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                slider.value += Time.deltaTime;
+                if (slider.value == 1)
+                {
+                    slider.gameObject.SetActive(false);
+                    moveStart = true;
+                    animator.SetBool("start", true);
+                }
+            }
+        }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
         {
+            slider.gameObject.SetActive(false);
             moveStart = false;
         }
     }
+
+
 }
---
+++ UI/BulletCount.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class BulletCount : MonoBehaviour
+{
+    public Text BulletCountText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BulletCountText.text = AutomaticGunScript.bullet_count.ToString() + "/50";
+    }
+}
