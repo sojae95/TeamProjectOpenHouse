9f43d65822fd3f78b6a226655d1f1fd17cfba089
sojae95 <37572033+sojae95@users.noreply.github.com>
2020-06-28 16:51:54 +0900

    Tutorial 수정 끝 밑 1스테이지 수정 미완

---
+++ Stage 1/Elevator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    bool moveStart = false;
+    Vector3 MoveDirection = new Vector3(0, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveStart == true)
+        {
+            MoveDirection = new Vector3(0, 1, 0);
+
+
+        }
+
+        this.transform.Translate(MoveDirection);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            moveStart = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            moveStart = false;
+        }
+    }
+}
+
