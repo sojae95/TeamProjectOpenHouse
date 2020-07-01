ce5d93427eaa40afe238f14fc4b0ea2a362b7213
Hyuneon <37572040+HyuneonY@users.noreply.github.com>
2020-07-01 16:42:08 +0900

    깃 오류 수정
    오류 수정

--- Stage 1/Eleva.cs
+++ Stage 1/Eleva.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Eleva : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class Eleva : MonoBehaviour
     public Transform MaxPos;
     public Transform MinPos;
     bool dirOfElevaY; //  true Up / false Down
+    public GameObject hostage;
 
     // Start is called before the first frame update
     void Start()
